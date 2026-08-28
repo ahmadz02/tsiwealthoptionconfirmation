2a86c7bd11ef8029ecd2ab131bdda1029b3561c8
ahmadz02 <129569697+ahmadz02@users.noreply.github.com>
2026-08-28 17:27:46 +0800

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,9 +1,11 @@
-const SUPABASE_URL = 'https://gtumqglezpgppfxqzwvi.supabase.co';
-const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_3YkAyb5TeyNWKvdBsS23yQ_ggTfqkgN';
+const SUPABASE_URL = 'https://ydpwrlwzmbloytjuuarx.supabase.co';
+
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_hJbKJq2avfol8PLLrVxJIg_A1ToPsdp';
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
-  SUPABASE_PUBLISHABLE_KEY,
+  SUPABASE_PUBLISHABLE_KEY
+);
   {
     auth: {
       persistSession: true,
